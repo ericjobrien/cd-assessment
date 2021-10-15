32891bbec7e9586ef830d65106da91c006a85eef
Eric O'Brien <obrienjeric@gmail.com>
2021-10-15 13:23:45 -0400

    finished all

---
+++ part3.js
@@ -0,0 +1,60 @@
+//Sum to zero
+let arr = [1, 2, 3, -2];
+
+function addToZero(arr) {
+let caseTrue = 0;
+let caseFalse = 0;
+
+for(let i = 0; i < arr.length; i++) {
+    for(let j = 0; j < arr.length; j++){
+      if(arr[i] + arr[j + 1] === 0) {
+        caseTrue ++;
+      } else {
+        caseFalse ++;
+      }
+    }
+  }
+
+if(caseTrue >= 1) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.time('sum')
+console.log(addToZero(arr));
+console.timeEnd('sum')
+
+//Run time is console loged as - 26.951ms - as a double loop this has a time complexity of O(n^2) - this is not an ideal solution
+// I would like to convert this to a sort method and give that a shot but I have zero experience with sort methods
+
+//below is an attempt to use a cloned array to test conditions
+
+let arr2 = [3, 5, 6]
+
+function sumQuicker(arr) {
+    let isTrue = 0;
+    let isFalse = 0;
+
+    let cloneArr = [...arr];
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] + cloneArr[i] === 0) {
+            isTrue ++;
+        } else {
+            isFalse ++
+        }
+    }
+    if(isTrue >= 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+console.time('quicker');
+console.log(sumQuicker(arr2));
+console.timeEnd('quicker');
+
+//turns out this is much faster
